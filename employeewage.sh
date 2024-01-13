60a22588d4f50ac6d0682273b47c5445c2e7372b
Rishbh Sinha <sinharishabh@gmail.com>
2024-01-13 12:41:08 +0530

    Feature added PartTimeWage

--- employeewage.sh
+++ employeewage.sh
@@ -5,6 +5,32 @@ then
 echo "employee is present"
 else
 echo "employee is absent"
+
+
+isDailyWage=1
+dailyhr=8
+hrSalary=20
+DailyempWage=$((dailyhr*hrSalary))
+echo  "$DailyempWage"
+
+
+isPartTime=1
+PartTimehr=8
+hrSalary=20
+TotalPartTimeWage=$((PartTimehr*hrSalary))
+echo  "$TotalPartTimeWage"
+TotalSalary=$((TotalPartTimeWage+DailyempWage))
+echo   "$TotalSalary"
+
 fi
+
+
+
+
+
+
+
+
+
 
 
